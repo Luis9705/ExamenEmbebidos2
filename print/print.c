7fe00ae1576bf9bf3ac141b49d114f52551e073f
JonaCB <jonacabrerab@hotmail.com>
2020-05-04 20:51:54 -0500

    Print documentation files

--- print/print.c
+++ print/print.c
@@ -1,13 +1,25 @@
-#include "print.h"
-#include "../uc_uart/uc_uart.h"
-#include "../miniprintf/miniprintf.h"
+/// @file print.c
+/// @brief Print functions definitions.
+//  Copyright 2020 Copyright Equipo 2
 
+#include "print.h" ///<Print header file.
+#include "../uc_uart/uc_uart.h" ///<Lower level of UART code include.
+#include "../miniprintf/miniprintf.h"   ///<Miniprint include.
+
+
+/**
+ * @brief Sets up parameters needed to print to console.
+ */
 void print_setup(void){
     uart_pin_setup(UART_TX_PORT, UART_TX_PIN, UART_RX_PORT, UART_RX_PIN);
     uart_setup(UART, BAUDRATE, DATABITS);
     uart_start(UART);
 }
 
+/**
+ * @brief Function to print to console.
+ * @param[in] format
+ */
 void print(const char *format, ...){
     va_list args;
 
--- print/print_params.h
+++ print/print_params.h
@@ -1,14 +1,16 @@
+/// @file print_params.h
+/// @brief Definition of parameters used to configure the UART.
 //  Copyright 2020 Copyright Equipo 2
 #ifndef PRINT_PARAMS_H_   /* Include guard */
 #define PRINT_PARAMS_H_
 
-#define UART_TX_PORT 1
-#define UART_TX_PIN 1
-#define UART_RX_PORT 1
-#define UART_RX_PIN 1
-#define UART 1
-#define BAUDRATE 115200
-#define DATABITS 8
+#define UART_TX_PORT 1  ///<UART Transmitter port used.
+#define UART_TX_PIN 1   ///<UART Transmitter pin used.
+#define UART_RX_PORT 1  ///<UART Receiver port used.
+#define UART_RX_PIN 1   ///<UART Receiver pin used.
+#define UART 1          ///<UART to be used.
+#define BAUDRATE 115200 ///<UART baudrate configuration.
+#define DATABITS 8      ///<UART data width configuration.
 
 
 #endif  // ABSTRACTION_AND_DOCUMENTATION_UC_TIMER_PRINT_PARAMS_H_
