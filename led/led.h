b8bd24e4dbbc9c3e5661d44fcbb8eec88f4bfa64
JonaCB <jonacabrerab@hotmail.com>
2020-05-04 20:31:52 -0500

    Documentation for UART

--- led/led.h
+++ led/led.h
@@ -1,16 +1,14 @@
+/// @file led.h
+/// @brief LED header file.
 //  Copyright 2020 Copyright Equipo 2
 #ifndef LED_H_   /* Include guard */
 #define LED_H_
 
-#include "led_params.h"
-
+#include "led_params.h" ///<LED params header include.
 
 void led_setup(void);
 void led_toggle(void);
 void led_on(void);
 void led_off(void);
-
-
-
 
 #endif  // ABSTRACTION_AND_DOCUMENTATION_LED_H_
--- led/led_params.h
+++ led/led_params.h
@@ -1,8 +1,10 @@
+/// @file led_params.h
+/// @brief LED PARAMS NEEDED
 //  Copyright 2020 Copyright Equipo 2
 #ifndef LED_PARAMS_H_   /* Include guard */
 #define LED_PARAMS_H_
 
-#define LED_PORT 1
-#define LED_PIN 1
+#define LED_PORT 1  ///<LED port to be used.
+#define LED_PIN 1   ///<LED pin to be used.
 
 #endif  // ABSTRACTION_AND_DOCUMENTATION_UC_TIMER_LED_PARAMS_H_
